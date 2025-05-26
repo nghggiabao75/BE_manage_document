8733f085ae16a120fe24ff121d7a3828224d68f6
Blaise <nghggiabao75@gmail.com>
2025-05-26 23:45:19 +0700

    #Update - Config

--- ManageDocument/ManageDocument/Program.cs
+++ ManageDocument/ManageDocument/Program.cs
@@ -6,7 +6,11 @@ using MediatR;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 // Configure MySQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
